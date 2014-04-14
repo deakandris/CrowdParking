b5fbe376e7b4d1a8b731c21a1e8d7eb0060980a8
András Deák <deaka8a@gmail.com>
2014-04-14 21:07:06 +0200

    Added gps location display.

--- Szakdolgozat/src/hu/bme/tmit/deakandras/crowdparking/MapFragment.java
+++ Szakdolgozat/src/hu/bme/tmit/deakandras/crowdparking/MapFragment.java
@@ -11,12 +11,17 @@ import org.mapsforge.map.reader.header.MapFileInfo;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.nutiteq.MapView;
 import com.nutiteq.components.Bounds;
@@ -25,26 +30,135 @@ import com.nutiteq.components.Components;
 import com.nutiteq.components.MapPos;
 import com.nutiteq.datasources.raster.MapsforgeRasterDataSource;
 import com.nutiteq.geometry.Line;
+import com.nutiteq.geometry.Marker;
 import com.nutiteq.geometry.Point;
-import com.nutiteq.geometry.Polygon;
 import com.nutiteq.log.Log;
 import com.nutiteq.projections.EPSG3857;
 import com.nutiteq.projections.EPSG4326;
 import com.nutiteq.rasterlayers.RasterLayer;
 import com.nutiteq.style.LineStyle;
+import com.nutiteq.style.MarkerStyle;
 import com.nutiteq.style.PointStyle;
 import com.nutiteq.style.PolygonStyle;
 import com.nutiteq.style.StyleSet;
 import com.nutiteq.ui.DefaultLabel;
+import com.nutiteq.ui.Label;
+import com.nutiteq.utils.Const;
 import com.nutiteq.utils.UnscaledBitmapLoader;
 import com.nutiteq.vectorlayers.GeometryLayer;
+import com.nutiteq.vectorlayers.MarkerLayer;
 
 public class MapFragment extends Fragment {
 
-	MapView mapView;
+	private Context context;
+	private MapView mapView;
+	private GeometryLayer locationLayer;
+	private LocationManager locationManager;
+	private LocationListener locationListener;
+	private Location myLocation;
 
 	public MapFragment() {
 
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		context = getActivity();
+
+		// Start the Location Service and binds a listener to it.
+		locationManager = (LocationManager) context
+				.getSystemService(Context.LOCATION_SERVICE);
+		locationListener = new LocationListener() {
+
+			@Override
+			public void onLocationChanged(Location location) {
+				if (location.getAccuracy() < 500) {
+					if (myLocation == null) {
+						Toast.makeText(
+								context,
+								R.string.location_found_jumping_to_your_location,
+								Toast.LENGTH_SHORT).show();
+					}
+					if (myLocation == null
+							|| location.getAccuracy() < myLocation
+									.getAccuracy()) {
+						myLocation = location;
+					}
+					
+					// jump to location
+					mapView.setFocusPoint(mapView
+							.getLayers()
+							.getBaseLayer()
+							.getProjection()
+							.fromWgs84(myLocation.getLongitude(), 
+									myLocation.getLatitude()));
+					
+					// draw location
+//					Bitmap pointMarker = UnscaledBitmapLoader.decodeResource(
+//							getResources(), R.drawable.point);
+//					PointStyle pointStyle = PointStyle.builder().setBitmap(pointMarker)
+//							.setSize(0.1f).setColor(Color.GREEN).build();
+//					locationLayer.add(new Point(new MapPos(19.040833, 47.498333),
+//							new DefaultLabel("Your Position"), pointStyle, null));
+					
+					drawLocation(myLocation);
+					
+					if (location.getAccuracy() < 5) {
+						locationManager.removeUpdates(this);
+					}
+				}
+			}
+
+			@Override
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+			}
+
+			@Override
+			public void onProviderEnabled(String provider) {
+				if (provider.equals(LocationManager.GPS_PROVIDER)) {
+					locationManager.requestLocationUpdates(
+							LocationManager.GPS_PROVIDER, 0, 0,
+							locationListener);
+					Toast.makeText(context, R.string.getting_your_location,
+							Toast.LENGTH_SHORT).show();
+				} else if (locationManager
+						.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+					locationManager.requestLocationUpdates(
+							LocationManager.NETWORK_PROVIDER, 0, 0,
+							locationListener);
+				}
+			}
+
+			@Override
+			public void onProviderDisabled(String provider) {
+				if (provider.equals(LocationManager.GPS_PROVIDER)) {
+					Toast.makeText(context, R.string.gps_is_disabled_toast,
+							Toast.LENGTH_LONG).show();
+				}
+			}
+
+		};
+
+		if (myLocation == null) {
+			if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+				locationManager.requestLocationUpdates(
+						LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+				Toast.makeText(context, R.string.getting_your_location,
+						Toast.LENGTH_SHORT).show();
+			} else {
+				Toast.makeText(context, R.string.gps_is_disabled_toast,
+						Toast.LENGTH_LONG).show();
+			}
+			if (locationManager
+					.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+				locationManager.requestLocationUpdates(
+						LocationManager.NETWORK_PROVIDER, 0, 0,
+						locationListener);
+			}
+		}
 	}
 
 	@Override
@@ -109,7 +223,7 @@ public class MapFragment extends Fragment {
 		// if no fileinfo, startPosition or boundingBox, then remain to default
 		// world view
 
-		// Activate some mapview options to make it smoother - optional
+		// Activate some mapview options to make it smoother
 		mapView.getOptions().setPreloading(false);
 		mapView.getOptions().setSeamlessHorizontalPan(true);
 		mapView.getOptions().setTileFading(false);
@@ -117,16 +231,17 @@ public class MapFragment extends Fragment {
 		mapView.getOptions().setDoubleClickZoomIn(true);
 		mapView.getOptions().setDualClickZoomOut(true);
 
-		// configure texture caching - optional, suggested
+		// configure texture caching
 		mapView.getOptions().setTextureMemoryCacheSize(20 * 1024 * 1024);
 		mapView.getOptions().setCompressedMemoryCacheSize(8 * 1024 * 1024);
+
 		return mapView;
 
 	}
 
 	private void createTestObjects() {
 		GeometryLayer geomLayer;
-		
+
 		geomLayer = new GeometryLayer(new EPSG4326());
 
 		// define minimum zoom for vector style visibility. If 0, then objects
@@ -192,14 +307,14 @@ public class MapFragment extends Fragment {
 
 		ArrayList<MapPos> outerPoses = new ArrayList<MapPos>();
 		for (double[] coord : pCoordsOuter) {
-			outerPoses.add(geomLayer.getProjection().fromWgs84((float) coord[0],
-					(float) coord[1]));
+			outerPoses.add(geomLayer.getProjection().fromWgs84(
+					(float) coord[0], (float) coord[1]));
 		}
 
 		ArrayList<MapPos> innerPoses = new ArrayList<MapPos>();
 		for (double[] coord : pCoordsInner) {
-			innerPoses.add(geomLayer.getProjection().fromWgs84((float) coord[0],
-					(float) coord[1]));
+			innerPoses.add(geomLayer.getProjection().fromWgs84(
+					(float) coord[0], (float) coord[1]));
 		}
 		// we need to create List of holes, as one polygon can have several
 		// holes
@@ -207,10 +322,63 @@ public class MapFragment extends Fragment {
 		List<List<MapPos>> holes = new ArrayList<List<MapPos>>();
 		holes.add(innerPoses);
 
-		//geomLayer.add(new Polygon(outerPoses, holes,
-		//		new DefaultLabel("Polygon"), polygonStyleSet, null));
-		
+		// geomLayer.add(new Polygon(outerPoses, holes,
+		// new DefaultLabel("Polygon"), polygonStyleSet, null));
+
 		mapView.getLayers().addLayer(geomLayer);
+	}
+
+	private void drawLocation(Location location) {
+		
+		// define marker style (image, size, color)
+        Bitmap pointMarker = UnscaledBitmapLoader.decodeResource(getResources(), R.drawable.olmarker);
+        MarkerStyle markerStyle = MarkerStyle.builder().setBitmap(pointMarker).setSize(0.5f).setColor(Color.WHITE).build();
+        // define label what is shown when you click on marker
+        Label markerLabel = new DefaultLabel("Your location", "Here is a marker");
+
+        // define location of the marker, it must be converted to base map coordinate system
+        MapPos markerLocation = mapView.getLayers()
+				.getBaseProjection().fromWgs84(location.getLongitude(), 
+						location.getLatitude());
+
+        // create layer and add object to the layer, finally add layer to the map
+        MarkerLayer markerLayer = new MarkerLayer(mapView.getLayers()
+				.getBaseProjection());
+        markerLayer.add(new Marker(markerLocation, markerLabel, markerStyle, markerLayer));
+        mapView.getLayers().addLayer(markerLayer);
+        
+		// create location circle
+		locationLayer = new GeometryLayer(mapView.getLayers()
+				.getBaseProjection());
+		circle(location.getLatitude(), location.getLongitude(), location.getAccuracy(), locationLayer);
+		mapView.getComponents().layers.addLayer(locationLayer);
+	}
+	
+	// helper to draw a circle to given layer
+	private void circle(double d, double e, float circleRadius,
+			GeometryLayer layer) {
+		// number of circle line points
+		int NR_OF_CIRCLE_VERTS = 18;
+		List<MapPos> circleVerts = new ArrayList<MapPos>(NR_OF_CIRCLE_VERTS);
+
+		MapPos circlePos = layer.getProjection().fromWgs84(d, e);
+		// width of map to scale circle
+		float projectionScale = (float) layer.getProjection().getBounds()
+				.getWidth();
+		// convert radius from meters to map units
+		float circleScale = circleRadius / 7500000f * projectionScale;
+
+		for (float tsj = 0; tsj <= 360; tsj += 360 / NR_OF_CIRCLE_VERTS) {
+			MapPos mapPos = new MapPos(circleScale
+					* Math.cos(tsj * Const.DEG_TO_RAD) + circlePos.x,
+					circleScale * Math.sin(tsj * Const.DEG_TO_RAD)
+							+ circlePos.y);
+			circleVerts.add(mapPos);
+		}
+		LineStyle style = LineStyle.builder().setWidth(0.1f)
+				.setColor(Color.argb(192, 255, 255, 0)).build();
+		Line circle = new Line(circleVerts, null, style, null);
+		layer.add(circle);
 	}
 
 	@Override
