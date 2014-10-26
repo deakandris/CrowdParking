84aa7c690afff24197e4ebb318af1c9f82e8c28d
deakandris <deaka8a@gmail.com>
2014-10-26 22:01:17 +0100

    Added loading screen and minor code changes to reflect the new sample data.

--- Szakdolgozat/src/hu/bme/tmit/deakandras/crowdparking/MapFragment.java
+++ Szakdolgozat/src/hu/bme/tmit/deakandras/crowdparking/MapFragment.java
@@ -4,6 +4,7 @@
 package hu.bme.tmit.deakandras.crowdparking;
 
 import hu.bme.tmit.deakandras.crowdparking.data.ParkingDataExtractor;
+import hu.bme.tmit.deakandras.crowdparking.data.ParkingDataProvider;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,6 +16,9 @@ import org.mapsforge.android.maps.mapgenerator.JobTheme;
 import org.mapsforge.map.reader.header.MapFileInfo;
 import org.xml.sax.SAXException;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
@@ -22,6 +26,8 @@ import android.graphics.Bitmap;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.LayoutInflater;
@@ -58,11 +64,53 @@ public class MapFragment extends Fragment {
 
 	private Context context;
 	private MapView mapView;
+	private View loadingView;
 	private GeometryLayer locationLayer;
 	private LocationManager locationManager;
 	private LocationListener locationListener;
 	private Location myLocation;
+	private StyleSet<PointStyle> pointStyleSet;
+	private StyleSet<LineStyle> lineStyleSet;
+	private StyleSet<PolygonStyle> polygonStyleSet;
+	private GeometryLayer geomLayer;
 
+	private class ParkingDataLoaderTask extends AsyncTask<Void, Void, List<ArrayList<MapPos>>> {
+
+		@Override
+		protected List<ArrayList<MapPos>> doInBackground(Void... params) {
+			List<ArrayList<MapPos>> roads = new ArrayList<ArrayList<MapPos>>();
+			try {
+				roads = ParkingDataExtractor.getWaysFromXML();
+			} catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return roads;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			showProgress(true);
+			super.onPreExecute();
+		}
+
+		@Override
+		protected void onPostExecute(List<ArrayList<MapPos>> result) {
+			for(ArrayList<MapPos> way : result){
+				
+				geomLayer.add(new Line(way, null, lineStyleSet, null));
+			}
+			showProgress(false);
+			super.onPostExecute(result);
+		}
+	}
+	
 	public MapFragment() {
 
 	}
@@ -92,25 +140,27 @@ public class MapFragment extends Fragment {
 									.getAccuracy()) {
 						myLocation = location;
 					}
-					
+
 					// jump to location
 					mapView.setFocusPoint(mapView
 							.getLayers()
 							.getBaseLayer()
 							.getProjection()
-							.fromWgs84(myLocation.getLongitude(), 
+							.fromWgs84(myLocation.getLongitude(),
 									myLocation.getLatitude()));
-					
+
 					// draw location
-//					Bitmap pointMarker = UnscaledBitmapLoader.decodeResource(
-//							getResources(), R.drawable.point);
-//					PointStyle pointStyle = PointStyle.builder().setBitmap(pointMarker)
-//							.setSize(0.1f).setColor(Color.GREEN).build();
-//					locationLayer.add(new Point(new MapPos(19.040833, 47.498333),
-//							new DefaultLabel("Your Position"), pointStyle, null));
-					
+					// Bitmap pointMarker = UnscaledBitmapLoader.decodeResource(
+					// getResources(), R.drawable.point);
+					// PointStyle pointStyle =
+					// PointStyle.builder().setBitmap(pointMarker)
+					// .setSize(0.1f).setColor(Color.GREEN).build();
+					// locationLayer.add(new Point(new MapPos(19.040833,
+					// 47.498333),
+					// new DefaultLabel("Your Position"), pointStyle, null));
+
 					drawLocation(myLocation);
-					
+
 					if (location.getAccuracy() < 5) {
 						locationManager.removeUpdates(this);
 					}
@@ -172,7 +222,8 @@ public class MapFragment extends Fragment {
 			Bundle savedInstanceState) {
 
 		View root = inflater.inflate(R.layout.fragment_map, container, false);
-
+		loadingView = root.findViewById(R.id.loading_screen);
+		
 		createMapView(root);
 		createTestObjects();
 
@@ -227,8 +278,10 @@ public class MapFragment extends Fragment {
 			}
 		}
 		// if no fileinfo, startPosition or boundingBox, then remain to default
-		// world view
-		
+		mapView.setFocusPoint(mapView.getLayers().getBaseLayer()
+				.getProjection().fromWgs84(19.0575, 47.4730));
+		mapView.setZoom(16);
+
 		// Activate some mapview options to make it smoother
 		mapView.getOptions().setPreloading(false);
 		mapView.getOptions().setSeamlessHorizontalPan(true);
@@ -245,14 +298,53 @@ public class MapFragment extends Fragment {
 
 	}
 
-	private void createTestObjects() {
-		GeometryLayer geomLayer;
+	/**
+	 * Shows the progress UI and hides the login form.
+	 */
+		@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+		private void showProgress(final boolean show) {
+			// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+			// for very easy animations. If available, use these APIs to fade-in
+			// the progress spinner.
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+				int shortAnimTime = getResources().getInteger(
+						android.R.integer.config_shortAnimTime);
 
+				loadingView.setVisibility(View.VISIBLE);
+				loadingView.animate().setDuration(shortAnimTime)
+						.alpha(show ? 1 : 0)
+						.setListener(new AnimatorListenerAdapter() {
+							@Override
+							public void onAnimationEnd(Animator animation) {
+								loadingView.setVisibility(show ? View.VISIBLE
+										: View.GONE);
+							}
+						});
+
+				mapView.setVisibility(View.VISIBLE);
+				mapView.animate().setDuration(shortAnimTime)
+						.alpha(show ? 0 : 1)
+						.setListener(new AnimatorListenerAdapter() {
+							@Override
+							public void onAnimationEnd(Animator animation) {
+								mapView.setVisibility(show ? View.VISIBLE
+										: View.VISIBLE);
+							}
+						});
+			} else {
+				// The ViewPropertyAnimator APIs are not available, so simply show
+				// and hide the relevant UI components.
+				loadingView.setVisibility(show ? View.VISIBLE : View.GONE);
+				mapView.setVisibility(show ? View.GONE : View.VISIBLE);
+			}
+		}
+	
+	private void createTestObjects() {
 		geomLayer = new GeometryLayer(new EPSG4326());
 
 		// define minimum zoom for vector style visibility. If 0, then objects
 		// are visible with any zoom.
-		int minZoom = 0;
+		int minZoom = 15;
 
 		// load bitmaps for vector elements. You can get the images from
 		// Hellomap3D project res/drawable
@@ -264,7 +356,7 @@ public class MapFragment extends Fragment {
 				R.drawable.line);
 
 		// set styles for all 3 object types: point, line and polygon
-		StyleSet<PointStyle> pointStyleSet = new StyleSet<PointStyle>();
+		pointStyleSet = new StyleSet<PointStyle>();
 		PointStyle pointStyle = PointStyle.builder().setBitmap(pointMarker)
 				.setSize(0.1f).setColor(Color.GREEN).build();
 		pointStyleSet.setZoomStyle(minZoom, pointStyle);
@@ -273,65 +365,59 @@ public class MapFragment extends Fragment {
 		// for nicer polylines
 		// Also do not forget to set Bitmap for Line. This allows to have fancy
 		// styles for lines.
-		StyleSet<LineStyle> lineStyleSet = new StyleSet<LineStyle>();
+		lineStyleSet = new StyleSet<LineStyle>();
 		lineStyleSet.setZoomStyle(minZoom,
 				LineStyle.builder().setBitmap(lineMarker).setWidth(0.1f)
 						.setColor(Color.GREEN).setPointStyle(pointStyle)
 						.build());
 		PolygonStyle polygonStyle = PolygonStyle.builder().setColor(Color.BLUE)
 				.build();
-		StyleSet<PolygonStyle> polygonStyleSet = new StyleSet<PolygonStyle>(
+		polygonStyleSet = new StyleSet<PolygonStyle>(
 				null);
 		polygonStyleSet.setZoomStyle(minZoom, polygonStyle);
 
 		geomLayer.add(new Point(new MapPos(19.055556, 47.481389),
-				new DefaultLabel("Budapesti Mûszaki és Gazdaságtudományi Egyetem"), pointStyle, null));
-		
-		try {
-			List<ArrayList<MapPos>> roads = ParkingDataExtractor.getWaysFromXML();
-			for(ArrayList<MapPos> way : roads){
-				geomLayer.add(new Line(way, null, lineStyleSet, null));
-			}
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+				new DefaultLabel(
+						"Budapesti Mûszaki és Gazdaságtudományi Egyetem"),
+				pointStyle, null));
 
+		new ParkingDataLoaderTask().execute(null, null, null);
+		
 		mapView.getLayers().addLayer(geomLayer);
 	}
 
 	private void drawLocation(Location location) {
-		
+
 		// define marker style (image, size, color)
-        Bitmap pointMarker = UnscaledBitmapLoader.decodeResource(getResources(), R.drawable.olmarker);
-        MarkerStyle markerStyle = MarkerStyle.builder().setBitmap(pointMarker).setSize(0.5f).setColor(Color.WHITE).build();
-        // define label what is shown when you click on marker
-        Label markerLabel = new DefaultLabel("Your location", "Here is a marker");
+		Bitmap pointMarker = UnscaledBitmapLoader.decodeResource(
+				getResources(), R.drawable.olmarker);
+		MarkerStyle markerStyle = MarkerStyle.builder().setBitmap(pointMarker)
+				.setSize(0.5f).setColor(Color.WHITE).build();
+		// define label what is shown when you click on marker
+		Label markerLabel = new DefaultLabel("Your location",
+				"Here is a marker");
 
-        // define location of the marker, it must be converted to base map coordinate system
-        MapPos markerLocation = mapView.getLayers()
-				.getBaseProjection().fromWgs84(location.getLongitude(), 
-						location.getLatitude());
+		// define location of the marker, it must be converted to base map
+		// coordinate system
+		MapPos markerLocation = mapView.getLayers().getBaseProjection()
+				.fromWgs84(location.getLongitude(), location.getLatitude());
 
-        // create layer and add object to the layer, finally add layer to the map
-        MarkerLayer markerLayer = new MarkerLayer(mapView.getLayers()
+		// create layer and add object to the layer, finally add layer to the
+		// map
+		MarkerLayer markerLayer = new MarkerLayer(mapView.getLayers()
 				.getBaseProjection());
-        markerLayer.add(new Marker(markerLocation, markerLabel, markerStyle, markerLayer));
-        mapView.getLayers().addLayer(markerLayer);
-        
+		markerLayer.add(new Marker(markerLocation, markerLabel, markerStyle,
+				markerLayer));
+		mapView.getLayers().addLayer(markerLayer);
+
 		// create location circle
 		locationLayer = new GeometryLayer(mapView.getLayers()
 				.getBaseProjection());
-		circle(location.getLatitude(), location.getLongitude(), location.getAccuracy(), locationLayer);
+		circle(location.getLatitude(), location.getLongitude(),
+				location.getAccuracy(), locationLayer);
 		mapView.getComponents().layers.addLayer(locationLayer);
 	}
-	
+
 	// helper to draw a circle to given layer
 	private void circle(double d, double e, float circleRadius,
 			GeometryLayer layer) {
--- Szakdolgozat/src/hu/bme/tmit/deakandras/crowdparking/data/ParkingDataExtractor.java
+++ Szakdolgozat/src/hu/bme/tmit/deakandras/crowdparking/data/ParkingDataExtractor.java
@@ -3,6 +3,8 @@ package hu.bme.tmit.deakandras.crowdparking.data;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
@@ -20,46 +22,79 @@ import com.nutiteq.components.MapPos;
 public class ParkingDataExtractor {
 	private static List<ArrayList<MapPos>> roads = new ArrayList<ArrayList<MapPos>>();
 
+	private static final Logger logger = Logger
+			.getLogger(ParkingDataExtractor.class.getName());
+
 	public static List<ArrayList<MapPos>> getWaysFromXML()
 			throws ParserConfigurationException, SAXException, IOException {
 
+		Logger.getLogger("").getHandlers()[0].setLevel(Level.ALL);
+		logger.setLevel(Level.FINE);
+		
 		SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
 		DefaultHandler handler = new DefaultHandler() {
 			ArrayMap<Long, MapPos> nodes = new ArrayMap<Long, MapPos>();
 			ArrayList<MapPos> nodeBuffer = new ArrayList<MapPos>();
 			List<ArrayList<MapPos>> ways = new ArrayList<ArrayList<MapPos>>();
+			int i = 0;
 
 			@Override
 			public void startElement(String uri, String localName,
 					String qName, Attributes attributes) throws SAXException {
+				logger.entering("DefaultHandler", "startElement",
+						new Object[] { uri, localName, qName, attributes });
 				if (qName.equalsIgnoreCase("node")) {
 					Long id = Long.parseLong(attributes.getValue("id"));
 					Float lat = Float.parseFloat(attributes.getValue("lat"));
 					Float lon = Float.parseFloat(attributes.getValue("lon"));
 					nodes.put(id, new MapPos(lon, lat));
+					logger.info("Node element found with ID=" + id);
 				} else if (qName.equalsIgnoreCase("nd")) {
 					Long ref = Long.parseLong(attributes.getValue("ref"));
-					nodeBuffer.add(nodes.get(ref));
+					if (nodes.get(ref) != null){
+						nodeBuffer.add(nodes.get(ref));
+						logger.info("Node reference found with ID=" + ref);
+					} else {
+						logger.info("Invalid node reference. ID=" + ref);
+					}
 				}
+				logger.exiting("DefaultHandler", "startElement",
+						new Object[] { uri, localName, qName, attributes });
 			}
 
 			@Override
 			public void endElement(String uri, String localName, String qName)
 					throws SAXException {
+				logger.entering("DefaultHandler", "endElement",
+						new Object[] { uri, localName, qName });
 				if (qName.equalsIgnoreCase("way")) {
-					ways.add(nodeBuffer);
-					nodeBuffer = new ArrayList<MapPos>();
+					if (nodeBuffer.size() > 1){
+						ways.add(nodeBuffer);
+						nodeBuffer = new ArrayList<MapPos>();
+						logger.info("Way element #"+ ++i + " added to ways.");
+					} else {
+						logger.info("Way contains less than 2 nodes, discard way.");
+					}
 				}
+				logger.exiting("DefaultHandler", "endElement",
+						new Object[] { uri, localName, qName });
 			}
 
 			@Override
 			public void endDocument() throws SAXException {
+				logger.entering("DefaultHandler", "endDocument");
 				roads = ways;
+				logger.info("Ended parsing ways.");
+				int i = 0;
+				for(ArrayList<MapPos> way : roads){
+					logger.info("#"+ ++i + " " + way);
+				}
+				logger.exiting("DefaultHandler", "endDocument");
 			}
 		};
 
 		saxParser.parse("file://" + Environment.getExternalStorageDirectory()
-				+ "/roadmap.xml", handler);
+				+ "/budapest_roads.osm", handler);
 		return roads;
 	}
 }
