0d3680584a73fc35cd76589d29716d1ee9154a41
deakandris <deaka8a@gmail.com>
2015-04-19 20:49:49 +0200

    organized imports

--- Szakdolgozat/src/hu/bme/tmit/deakandras/crowdparking/gui/MainActivity.java
+++ Szakdolgozat/src/hu/bme/tmit/deakandras/crowdparking/gui/MainActivity.java
@@ -2,19 +2,13 @@ package hu.bme.tmit.deakandras.crowdparking.gui;
 
 import hu.bme.tmit.deakandras.crowdparking.R;
 import hu.bme.tmit.deakandras.crowdparking.gui.map.MapFragment;
-import android.app.Activity;
-import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 	
